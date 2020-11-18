37a55bfd4dfa61c2822eee7c4d130c39d0c2616d
ivanbudnick <ivan.budnick@gmail.com>
2020-11-18 17:54:17 -0300

    Prueba desde casa

--- script.js
+++ script.js
@@ -19,32 +19,38 @@ yellow.addEventListener("click", changeColorYellow);
 // -----------------FUNCTIONS
 
 function startGame(e) {
-    console.log("hola");
-    
+
+    blue.classList.toggle("fancyBlue");
+    setTimeout(function prueba(e) {
+        blue.classList.remove("fancyBlue")
+    }, 1000);
+
 }
 
 function changeColorBlue(e) {
     blue.classList.toggle("fancyBlue");
     setTimeout(function prueba(e) {
-        blue.classList.remove("fancyBlue")},1000
-    );
+        blue.classList.remove("fancyBlue")
+    }, 1000);
 }
+
 function changeColorRed(e) {
     red.classList.toggle("fancyRed");
     setTimeout(function prueba(e) {
-        red.classList.remove("fancyRed")},1000
-    );
+        red.classList.remove("fancyRed")
+    }, 1000);
 }
+
 function changeColorGreen(e) {
     green.classList.toggle("fancyGreen");
     setTimeout(function prueba(e) {
-        green.classList.remove("fancyGreen")},1000
-    );
+        green.classList.remove("fancyGreen")
+    }, 1000);
 }
+
 function changeColorYellow(e) {
     yellow.classList.toggle("fancyYellow");
     setTimeout(function prueba(e) {
-        yellow.classList.remove("fancyYellow")},1000
-    );
+        yellow.classList.remove("fancyYellow")
+    }, 1000);
 }
-
